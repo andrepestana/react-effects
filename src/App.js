ea37af9d8f3f5386ad27fc410eb90979f342fab4
andrepestana <andrepestana@gmail.com>
2020-11-12 00:04:22 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./avatar.jpg";
+import balloon from "./Speech_balloon.svg";
+import "./App.css";
+import Typing from 'react-typing-animation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <img id="avatar" src={logo} alt="logo" />
+        <img id="balloon" src={balloon} alt="balloon" />
+        <div id="balloon-text">
+          <Typing><b>Hi there! <br />My name is Andre Pestana!<br /> Welcome to my website!</b></Typing>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './CRIMFBRG.TTF';
 
 ReactDOM.render(
   <React.StrictMode>
