2a375f85562aed89de3f2f2b0e047d98d2c59a99
Andre Pestana <andre@Andres-MacBook-Pro.local>
2020-11-21 12:29:04 -0800

    fixed effect cross-browsers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import logo from "./avatar.jpg";
-import balloon from "./Speech_balloon.svg";
+import balloon from "./Speech_balloon_distorted.png";
 import "./App.css";
 import Typing from 'react-typing-animation';
 
 function App() {
   return (
     <div className="App">
+      <h1 id="title">ANDRE PESTANA</h1>
       <div id="container">
-        <img id="avatar" src={logo} alt="logo" />
+        <div id="avatar-container">
+          <img id="avatar" src={logo} alt="logo" />
+        </div>
         <img id="balloon" src={balloon} alt="balloon" />
         <div id="balloon-text">
-          <Typing><b>Hi there! <br />My name is Andre Pestana!<br /> Welcome to my website!</b></Typing>
+          <Typing startDelay="2000">
+            <b>Hi there! <br />My name is Andre Pestana!<br /> Welcome to my website!</b>
+            <Typing.Delay ms={2000} />
+            <Typing.Backspace count={60} speed={20} />
+            <Typing.Speed ms={50} />
+            <b>Take a look at <br />my projects and about me.<br /> Drop me a line in case you <br />have any questions!</b>
+          </Typing>
+        </div>
+      </div>
+      <div id="container-frames">
+        <div id="frame"></div>
+        <div id="frame2">
+          <span id="box1"></span>
+          <span id="box2"></span>
+          <span id="box3"></span>
+          <span id="box4"></span>
+          <span id="box5"></span>
+          <span id="box6"></span>
+          <span id="box7"></span>
+          <span id="box8">
+            <span class="w"></span>
+            <span class="w"></span>
+          </span>
+          <span id="box9"></span>
+          <span id="box10"></span>
         </div>
       </div>
     </div>
